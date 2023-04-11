cf98b24c4120ade69092b278bfdf177b919a5cc4
oscar <dadada>
2023-04-11 14:40:34 +0200

    fixed now!!!

--- lib/screens/segundapantalla.dart
+++ lib/screens/segundapantalla.dart
@@ -22,69 +22,88 @@ class _TransferenciaScreenState extends State<TransferenciaScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Transferencias'),
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey[900],
       ),
-      body: FutureBuilder(
-        future: _transferencias,
-        builder: (BuildContext context,
-            AsyncSnapshot<List<Transferencia>> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Text("${snapshot.error}"),
-            );
-          } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
-            return GridView.count(
-              crossAxisCount: 2,
-              childAspectRatio: 1,
-              mainAxisSpacing: 10,
-              crossAxisSpacing: 10,
-              padding: EdgeInsets.all(10),
-              children: snapshot.data!.map((transferencia) {
-                return GestureDetector(
-                  onTap: () async {
-                    String result = await transferItem(
-                        transferencia,
-                        transferencia.characterId,
-                        transferencia.itemInstanceId,
-                        transferencia.membershipType,
-                        transferencia.itemReferenceHash,
-                        transferencia.stackSize,
-                        transferencia.transferToVault,
-                        transferencia.itemName);
-                    ScaffoldMessenger.of(context)
-                        .showSnackBar(SnackBar(content: Text(result)));
-                  },
-                  child: Card(
-                    child: Column(
-                      children: [
-                        Expanded(
-                          child: Image.network(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: Image.network(
+                    'https://th.bing.com/th/id/OIP.BszFeHpWQrTyMTNSFOxLNwHaEo?pid=ImgDet&rs=1')
+                .image,
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: FutureBuilder(
+          future: _transferencias,
+          builder: (BuildContext context,
+              AsyncSnapshot<List<Transferencia>> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text("${snapshot.error}"),
+              );
+            } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+              return GridView.count(
+                crossAxisCount: 2,
+                childAspectRatio: 1,
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 10,
+                padding: EdgeInsets.all(10),
+                children: snapshot.data!.map((transferencia) {
+                  return GestureDetector(
+                    onTap: () async {
+                      String result = await transferItem(
+                          transferencia,
+                          transferencia.characterId,
+                          transferencia.itemInstanceId,
+                          transferencia.membershipType,
+                          transferencia.itemReferenceHash,
+                          transferencia.stackSize,
+                          transferencia.transferToVault,
+                          transferencia.itemName);
+                      ScaffoldMessenger.of(context)
+                          .showSnackBar(SnackBar(content: Text(result)));
+                    },
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      elevation: 5,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.network(
                             'https://www.bungie.net${transferencia.icon}',
+                            height: 80,
+                            width: 80,
                             fit: BoxFit.contain,
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
+                          SizedBox(height: 10),
+                          Text(
                             transferencia.itemName,
                             textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                );
-              }).toList(),
-            );
-          } else {
-            return Center(
-              child: Text("No hay datos disponibles"),
-            );
-          }
-        },
+                  );
+                }).toList(),
+              );
+            } else {
+              return Center(
+                child: Text("No hay datos disponibles"),
+              );
+            }
+          },
+        ),
       ),
     );
   }
